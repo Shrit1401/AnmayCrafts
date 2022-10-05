8d81eb05ee00e2a4a189d2557d05624f50675893
Shrit Shrivastava <67160842+Shrit1401@users.noreply.github.com>
2022-10-05 17:13:56 +0530

    First commit

--- custom.js
+++ custom.js
@@ -1,3 +1,8 @@
-document.querySelector('.js-burger-btn').addEventListener('click', function() {
+document.querySelector('.js-burger-btn').addEventListener('click', function () {
     document.querySelector('.c-mobileNav').classList.toggle('is-active');
 })
+
+//toggle is-active class 
+document.querySelector(".js-products-btn").addEventListener("click", function () {
+    document.querySelector(".c-products").classList.toggle("is-active");
+});
